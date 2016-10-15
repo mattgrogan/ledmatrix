93be78ba4b41de93df9ab2abf857720d047e2bf5
Matthew Grogan <mvgnyc@gmail.com>
2016-10-15 12:19:19 +0000

    Added multiple menus

--- ledmatrix/main.py
+++ ledmatrix/main.py
@@ -1,7 +1,17 @@
-# Main file for ledmatrix
+from gif_playlist import Gif_Playlist
 from main_controller import Main_Controller
+from rgbmatrix import Adafruit_RGBmatrix
 
 if __name__ == "__main__":
 
+  matrix = Adafruit_RGBmatrix(32, 1)
+
   controller = Main_Controller()
+
+  gengifs_folder = "/home/pi/github/ledmatrix/icons/gifs"
+  controller.add_menu_item(Gif_Playlist(gengifs_folder, matrix))
+
+  halogifs_folder = "/home/pi/github/ledmatrix/icons/halogifs"
+  controller.add_menu_item(Gif_Playlist(halogifs_folder, matrix))
+
   controller.run()
--- ledmatrix/main_controller.py
+++ ledmatrix/main_controller.py
@@ -1,9 +1,6 @@
 import time
 
 import lirc
-from gif_player import Gif_Player
-from gif_playlist import Gif_Playlist
-from rgbmatrix import Adafruit_RGBmatrix
 
 
 class Main_Controller(object):
@@ -12,28 +9,53 @@ class Main_Controller(object):
   def __init__(self):
     """ Initialize the controller """
 
-    self.matrix = Adafruit_RGBmatrix(32, 1)
-
     # Initialize the remote control
     self.remote_controller = lirc.init(
         "ledmatrix", "./lircrc", blocking=False)
 
+    self.menu_items = []
+    self.current_index = 0
+
+  @property
+  def current_item(self):
+    """ Return the current item """
+
+    if len(self.menu_items) == 0:
+      raise ValueError("You must add menu items to the controller")
+
+    return self.menu_items[self.current_index]
+
+  def add_menu_item(self, menu_item):
+    """ Add a menu item to the list """
+
+    self.menu_items.append(menu_item)
+
+  def move(self, step=1):
+    """ Move to next menu item """
+
+    self.current_index += step
+
+    if self.current_index >= len(self.menu_items):
+      self.current_index = 0
+    elif self.current_index < 0:
+      self.current_index = len(self.menu_items) - 1
+
   def run(self):
     """ Run the animations """
 
-    folder = "/home/pi/github/ledmatrix/icons/gifs"
-
-    gif_player = Gif_Playlist(folder, self.matrix)
-
     while True:
 
-      requested_delay = gif_player.draw_frame()
+      requested_delay = self.current_item.draw_frame()
 
       time.sleep(requested_delay)
 
       code = lirc.nextcode()
 
       if len(code) > 0 and code[0] == u"KEY_RIGHT":
-        gif_player.move(1)
+        self.current_item.move(1)
       elif len(code) > 0 and code[0] == u"KEY_LEFT":
-        gif_player.move(-1)
+        self.current_item.move(-1)
+      elif len(code) > 0 and code[0] == u"KEY_DOWN":
+        self.move(1)
+      elif len(code) > 0 and code[0] == u"KEY_UP":
+        self.move(-1)
