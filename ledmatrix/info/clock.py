ebb108c8aff8ac1cfbd39fc15c9c014a32502ca0
Matthew Grogan <mvgnyc@gmail.com>
2016-11-06 18:45:39 -0500

    renamed clock

---
+++ ledmatrix/info/clock.py
@@ -0,0 +1,70 @@
+import os
+import time
+
+import randomcolor
+from PIL import Image, ImageChops, ImageDraw, ImageFont
+
+FONTFILE = os.path.join(os.path.dirname(__file__), "../../fonts/RPGSystem.ttf")
+FONTSIZE = 16
+
+SMALLFONT = os.path.join(os.path.dirname(
+    __file__), "../../fonts/small_pixel.ttf")
+SMALLFONTSIZE = 8
+
+
+class Clock(object):
+  """ Show the time and date """
+
+  def __init__(self, width, height):
+    """ Initialize the player """
+
+    self.width = width
+    self.height = height
+
+    self.font = ImageFont.truetype(FONTFILE, FONTSIZE)
+    self.small_font = ImageFont.truetype(SMALLFONT, SMALLFONTSIZE)
+
+    self.time_color = None
+    self.date_color = None
+    self.old_time = None
+
+    self.is_finished = False
+
+  def randomize_colors(self, new_time=None, force=False):
+    """ Pick some random colors """
+
+    if new_time != self.old_time or force:
+
+      rand_color = randomcolor.RandomColor()
+
+      self.time_color = rand_color.generate()[0]
+      self.date_color = rand_color.generate()[0]
+
+      self.old_time = new_time
+
+  def move(self, step=None):
+    """ Respond to the right and left remote buttons. Step value is ignored """
+
+    self.randomize_colors(force=True)
+
+  def draw_frame(self):
+    """ Draw the time on the screen """
+
+    image = Image.new("RGB", (self.width, self.height))
+    draw = ImageDraw.Draw(image)
+
+    time_str = time.strftime("%I:%M", time.localtime()).lstrip("0")
+    self.randomize_colors(time_str)
+
+    w, h = self.small_font.getsize(time_str)
+    xloc = (self.width - w) / 2
+
+    draw.text((xloc, 2), time_str, font=self.small_font, fill=self.time_color)
+
+    day_str = time.strftime("%a", time.localtime())
+    draw.text((1, 12), day_str, font=self.small_font, fill=self.date_color)
+
+    date_str = time.strftime("%b %d", time.localtime())
+    draw.text((1, 20), date_str, font=self.small_font, fill=self.date_color)
+
+    return image, 1
--- ledmatrix/main.py
+++ ledmatrix/main.py
@@ -3,8 +3,8 @@ import os
 import time
 
 from animation import Gif_Playlist
+from info import Clock
 from main_controller import Main_Controller
-from time_player import Time_Player
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 GENGIFS_FOLDER = os.path.normpath(os.path.join(current_dir, "../icons/gifs/"))
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
   controller = Main_Controller()
 
-  controller.add_menu_item(Time_Player(32, 32))
+  controller.add_menu_item(Clock(32, 32))
   controller.add_menu_item(Gif_Playlist(GENGIFS_FOLDER, timeout_ms=10000))
 
   # Running on the Raspberry Pi
