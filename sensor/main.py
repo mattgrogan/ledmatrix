8fab263ea44a754dbd2be385b747aa5c7748d2a9
Matthew Grogan <mvgnyc@gmail.com>
2016-12-12 14:20:42 -0500

    Added sensors

--- sensor/main.py
+++ sensor/main.py
@@ -1,4 +1,5 @@
 
+from components.ambient_light import Ambient_Light_Sensor
 from components.ir_remote import IR_Remote
 from components.temp import Temperature_Sensor_BMP280
 from sensor_controller import Sensor_Controller
@@ -13,8 +14,10 @@ if __name__ == "__main__":
 
   ir_remote = IR_Remote(host=ZMQ_HOST)
   temp_sensor = Temperature_Sensor_BMP280(dbhost=INFLUX_HOST, dbname=INFLUX_DB)
+  ambient_light = Ambient_Light_Sensor(dbhost=INFLUX_HOST, dbname=INFLUX_DB)
 
   controller.add_sensor(ir_remote)
   controller.add_sensor(temp_sensor)
+  controller.add_sensor(ambient_light)
 
   controller.mainloop()
