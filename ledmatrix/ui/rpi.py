63bdb1b5bf314586eb4df9a650c9c980e4501659
Matthew Grogan <mvgnyc@gmail.com>
2017-02-04 07:52:05 -0500

    Simplified buttons

--- ledmatrix/ui/rpi.py
+++ ledmatrix/ui/rpi.py
@@ -1,8 +1,11 @@
-from __future__ import division, print_function
+from __future__ import division
 
 import sys
 import time
 
+from gpiozero import Button
+
+BUTTON_GPIO_PIN = 24
 TICK_MS = 10.0
 
 
@@ -12,39 +15,39 @@ class Rpi_UI(object):
 
     self.controller = controller
 
+    # Set up the matrix
     from device import RGB_Matrix
     self.controller.matrix = RGB_Matrix()
 
+    # Handle control messages
     from zmq_control import ZeroMQ_Control
-    self.rc = ZeroMQ_Control()
-    from button_control import Button_Control
-    self.rc2 = Button_Control()
+    self.zmq = ZeroMQ_Control()
 
-    self.rc.register("KEY_PLAYPAUSE", controller, controller.handle_playpause)
-    self.rc.register("KEY_STOP", controller, controller.handle_mode)
-    self.rc.register("KEY_UP", controller, controller.handle_up)
-    self.rc.register("KEY_DOWN", controller, controller.handle_down)
-    self.rc.register("KEY_LEFT", controller, controller.handle_left)
-    self.rc.register("KEY_RIGHT", controller, controller.handle_right)
+    self.zmq.register("KEY_PLAYPAUSE", controller, controller.handle_playpause)
+    self.zmq.register("KEY_STOP", controller, controller.handle_mode)
+    self.zmq.register("KEY_UP", controller, controller.handle_up)
+    self.zmq.register("KEY_DOWN", controller, controller.handle_down)
+    self.zmq.register("KEY_LEFT", controller, controller.handle_left)
+    self.zmq.register("KEY_RIGHT", controller, controller.handle_right)
+
+    # Handle the physical button
+    self.button = Button(BUTTON_GPIO_PIN)
+    self.button.when_released = self.controller.handle_mode
 
   def mainloop(self):
 
     while True:
-      # rc2 is a hack for now. Can't get lirc to work.
-      self.rc2.read_command(self.controller.is_running)
-      self.rc.read_command()
+      # Have we received any zmq messages?
+      self.zmq.read_command()
 
+      # Run this iteration
       requested_delay_ms = self.controller.run()
 
-      if requested_delay_ms < TICK_MS:
-        requested_delay_ms = TICK_MS
+      # Round up to the tick
+      requested_delay_ms = max(requested_delay_ms, TICK_MS)
 
-      current_timeout_ms = 0
-
-      while current_timeout_ms < requested_delay_ms:
-        self.rc2.read_command(self.controller.is_running)
-        self.rc.read_command()
+      # Hold in small increments until we've reached requested_delay_ms
+      current_delay_ms = 0
+      while current_delay_ms < requested_delay_ms:
         time.sleep(TICK_MS / 1000.0)
-        current_timeout_ms += TICK_MS
-
-      #time.sleep(requested_delay_ms / 1000.0)
+        current_delay_ms += TICK_MS
