bf72903a24ddd43b84c7d8d78c96cbfa33fc2c85
Matthew Grogan <mvgnyc@gmail.com>
2017-03-19 18:56:09 -0400

    Moved indicatorapp to new file

--- ledmatrix/components/__init__.py
+++ ledmatrix/components/__init__.py
@@ -5,7 +5,8 @@ from viewport import Viewport_Mixin, Viewport_NoScroll_Mixin
 from text import Text, NoScroll_Text
 from icon import Icon
 
-from app import App, Indicator_App
+from app import App
+from indicator_app import Indicator_App
 from indicator_image import Indicator_Image
 
 from indicator_frame import Indicator_Frame
